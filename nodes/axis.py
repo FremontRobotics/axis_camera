c35c035f100ec5df86c3fee3668d580844a818cc
Jason Kelly <3310379+JasonLKelly@users.noreply.github.com>
2023-11-15 11:36:16 -0800

    Updated Axis node

--- nodes/axis.py
+++ nodes/axis.py
@@ -9,13 +9,16 @@ import threading
 import urllib.request, urllib.error, urllib.parse
 import requests, requests.auth
 import datetime
+import math
 import time
+import sys
 
-import rospy
+import rclpy
+from rclpy.node import Node
 from sensor_msgs.msg import CompressedImage, CameraInfo
 from std_msgs.msg import Bool
-from std_srvs.srv import SetBool, SetBoolRequest, SetBoolResponse
-import camera_info_manager
+#from std_srvs.srv import SetBool, SetBoolRequest, SetBoolResponse
+#import camera_info_manager
 
 class StreamThread(threading.Thread):
     def __init__(self, axis):
@@ -34,18 +37,18 @@ class StreamThread(threading.Thread):
             self.authenticate()
             if self.openURL():
                 self.publishFramesContinuously()
-            rospy.sleep(2) # if stream stays intact we shouldn't get to this
+            time.sleep(2) # if stream stays intact we shouldn't get to this
 
     def formURL(self):
         self.url = 'http://%s/mjpg/video.mjpg' % self.axis.hostname
-        self.url += "?fps=%d&resolution=%dx%d" % (self.axis.fps, self.axis.width,
-                                                            self.axis.height)
+        #self.url += "?fps=%d&resolution=%dx%d" % (self.axis.fps, self.axis.width, self.axis.height)
+
 
         # support for Axis F34 multicamera switch
         if (self.axis.camera != 0):
             self.url += "&camera=%s" % str(self.axis.camera)
 
-        rospy.logdebug('opening ' + str(self.axis))
+        print('opening ' + str(self.url))
 
     def authenticate(self):
         '''only try to authenticate if user/pass configured.  I have not
@@ -75,7 +78,7 @@ class StreamThread(threading.Thread):
             self.fp = urllib.request.urlopen(self.url, timeout=self.timeoutSeconds)
             return(True)
         except urllib.error.URLError as e:
-            rospy.logwarn('Error opening URL %s' % (self.url) +
+            print('Error opening URL %s' % (self.url) +
                             'Possible timeout.  Looping until camera appears')
             return(False)
 
@@ -88,8 +91,8 @@ class StreamThread(threading.Thread):
                 self.publishMsg()
                 self.publishCameraInfoMsg()
 
-            except:
-                rospy.logwarn('Timed out while trying to get message.')
+            except Exception as err:
+                print(f"Exception publishing frames: {err}")
                 break
 
     def findBoundary(self):
@@ -116,7 +119,7 @@ class StreamThread(threading.Thread):
             try:
                 self.header[parts[0]] = parts[1]
             except:
-                rospy.logwarn('Problem encountered with image header.  Setting '
+                print('Problem encountered with image header.  Setting '
                                                     'content_length to zero')
                 self.header[b'Content-Length'] = 0 # set content_length to zero if
                                             # there is a problem reading header
@@ -131,34 +134,67 @@ class StreamThread(threading.Thread):
     def publishMsg(self):
         '''Publish jpeg image as a ROS message'''
         self.msg = CompressedImage()
-        self.msg.header.stamp = rospy.Time.now()
-        self.msg.header.frame_id = self.axis.frame_id
+        self.msg.header.stamp = self.axis.get_clock().now().to_msg()
+        self.msg.header.frame_id = self.axis.frame_id   
         self.msg.format = "jpeg"
         self.msg.data = self.img
         self.axis.pub.publish(self.msg)
 
     def publishCameraInfoMsg(self):
         '''Publish camera info manager message'''
-        cimsg = self.axis.cinfo.getCameraInfo()
-        cimsg.header.stamp = self.msg.header.stamp
-        cimsg.header.frame_id = self.axis.frame_id
-        cimsg.width = self.axis.width
-        cimsg.height = self.axis.height
-        self.axis.caminfo_pub.publish(cimsg)
+        camera_info = CameraInfo()
+        camera_info.distortion_model = 'plumb_bob'
+        camera_info.header.stamp = self.msg.header.stamp
+        camera_info.header.frame_id = self.axis.frame_id
+        camera_info.width = self.axis.width
+        camera_info.height = self.axis.height
 
-class Axis:
-    def __init__(self, hostname, username, password, width, height, fps, frame_id,
-                 camera_info_url, use_encrypted_password, camera, ir, defog, wiper):
-        self.hostname = hostname
-        self.username = username
-        self.password = password
-        self.width = width
-        self.height = height
-        self.fps = fps
-        self.frame_id = frame_id
-        self.camera_info_url = camera_info_url
-        self.use_encrypted_password = use_encrypted_password
-        self.camera = camera
+        cx = camera_info.width / 2.0
+        cy = camera_info.height / 2.0
+        fx = camera_info.width / (
+            2.0 * math.tan(float(61) * math.pi / 360.0))   # TODO: Hardcoded FOV
+        fy = fx
+        camera_info.k = [fx, 0.0, cx, 0.0, fy, cy, 0.0, 0.0, 1.0]
+        camera_info.d = [0.0, 0.0, 0.0, 0.0, 0.0]
+        camera_info.r = [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]
+        camera_info.p = [fx, 0.0, cx, 0.0, 0.0, fy, cy, 0.0, 0.0, 0.0, 1.0, 0.0]
+
+        self.axis.caminfo_pub.publish(camera_info)
+
+class Axis(Node):
+    def __init__(self):
+        
+        super().__init__('axis')
+    
+        self.get_logger().info(f"Starting node")
+        self.declare_parameter('hostname', '192.168.0.228:8000') # default IP address
+        self.declare_parameter('username', '')         # default login name
+        self.declare_parameter('password', '')
+        self.declare_parameter('width', 640)
+        self.declare_parameter('height', 480)
+        self.declare_parameter('fps', 0)                   # frames per second (0 = camera default)
+        self.declare_parameter('frame_id', 'axis_camera')
+        self.declare_parameter('camera_info_url', '')
+        self.declare_parameter('use_encrypted_password', False)
+        self.declare_parameter('camera', 0)
+        self.declare_parameter('ir', False)
+        self.declare_parameter('defog', False)
+        self.declare_parameter('wiper', False)
+    
+        
+        self.hostname = self.get_parameter('hostname').value
+        self.username = self.get_parameter('username').value
+        self.password = self.get_parameter('password').value
+        self.width = self.get_parameter('width').value
+        self.height = self.get_parameter('height').value
+        self.fps = self.get_parameter('fps').value
+        self.frame_id = self.get_parameter('frame_id').value
+        self.camera_info_url = self.get_parameter('camera_info_url').value
+        self.use_encrypted_password = self.get_parameter('use_encrypted_password').value
+        self.camera = self.get_parameter('camera').value
+        self.ir = self.get_parameter('ir').value
+        self.defog = self.get_parameter('defog').value
+        self.wiper = self.get_parameter('wiper').value
 
         self.http_headers = {
             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
@@ -171,58 +207,61 @@ class Axis:
         self.http_timeout = (3, 5)
 
         # generate a valid camera name based on the hostname
-        self.cname = camera_info_manager.genCameraName(self.hostname)
-        self.cinfo = camera_info_manager.CameraInfoManager(cname = self.cname,
-                                                   url = self.camera_info_url)
-        self.cinfo.loadCameraInfo()         # required before getCameraInfo()
+        self.cname = f"AXIS_{self.hostname}"  #TODO: Improve this 
+        
+        self.get_logger().info(f"Camera name: {self.cname}")
+
+        #self.cinfo.loadCameraInfo()         # required before getCameraInfo()
         self.st = None
-        self.pub = rospy.Publisher("image_raw/compressed", CompressedImage, self, queue_size=1)
-        self.caminfo_pub = rospy.Publisher("camera_info", CameraInfo, self, queue_size=1)
+
+        self.pub = self.create_publisher(CompressedImage, "image_raw/compressed", 10)
+        self.caminfo_pub = self.create_publisher(CameraInfo, "image_raw/camera_info", 10)
+
+        if self.st is None:
+            self.st = StreamThread(self)
+            self.get_logger().warn(f"Starting streamthread image-publisher.")
+            self.st.start()
 
         # The Axis Q62 series supports a night-vision mode with an active IR illuminator
         # If this option is enabled, add the necessary services and topics
-        if ir:
-            self.ir_on = False
-            self.ir_on_off_srv = rospy.Service('set_ir_on', SetBool, self.handle_toggle_ir)
-            self.ir_on_pub = rospy.Publisher('ir_on', Bool, queue_size=1)
-            self.ir_on_pub_thread = threading.Thread(target=self.ir_on_pub_thread_fn)
-            self.ir_on_pub_thread.start()
-
-            self.handle_toggle_ir(SetBoolRequest(False))
+        #if self.ir:
+            #self.ir_on = False
+            #self.ir_on_off_srv = rospy.Service('set_ir_on', SetBool, self.handle_toggle_ir)
+            #self.ir_on_pub = rospy.Publisher('ir_on', Bool, queue_size=1)
+            #self.ir_on_pub_thread = threading.Thread(target=self.ir_on_pub_thread_fn)
+            #self.ir_on_pub_thread.start()
+#
+            #self.handle_toggle_ir(SetBoolRequest(False))
 
         # The Axis Q62 series is equipped with a wiper on the camera lens
         # If this option is enabled, add the necessary services and topics
-        if wiper:
-            self.wiper_on_time = datetime.datetime.utcnow()
-            self.wiper_on = False
-            self.wiper_on_off_srv = rospy.Service('set_wiper_on', SetBool, self.handle_toggle_wiper)
-            self.wiper_on_pub = rospy.Publisher('wiper_on', Bool, queue_size=1)
-            self.wiper_on_pub_thread = threading.Thread(target=self.wiper_on_pub_thread_fn)
-            self.wiper_on_pub_thread.start()
-
-            self.handle_toggle_wiper(SetBoolRequest(False))
+        #if self.wiper:
+            #self.wiper_on_time = datetime.datetime.utcnow()
+            #self.wiper_on = False
+            #self.wiper_on_off_srv = rospy.Service('set_wiper_on', SetBool, self.handle_toggle_wiper)
+            #self.wiper_on_pub = rospy.Publisher('wiper_on', Bool, queue_size=1)
+            #self.wiper_on_pub_thread = threading.Thread(target=self.wiper_on_pub_thread_fn)
+            #self.wiper_on_pub_thread.start()
+#
+            #self.handle_toggle_wiper(SetBoolRequest(False))
 
         # The Axis Q62 series is equipped with a defogger
         # If this option is enabled, add the necessary services and topics
-        if defog:
-            self.defog_on = False
-            self.defog_on_off_srv = rospy.Service('set_defog_on', SetBool, self.handle_toggle_defog)
-            self.defog_on_pub = rospy.Publisher('defog_on', Bool, queue_size=1)
-            self.defog_on_pub_thread = threading.Thread(target=self.defog_on_pub_thread_fn)
-            self.defog_on_pub_thread.start()
-
-            self.handle_toggle_defog(SetBoolRequest(False))
+        #if self.defog:
+            #self.defog_on = False
+            #self.defog_on_off_srv = rospy.Service('set_defog_on', SetBool, self.handle_toggle_defog)
+            #self.defog_on_pub = rospy.Publisher('defog_on', Bool, queue_size=1)
+            #self.defog_on_pub_thread = threading.Thread(target=self.defog_on_pub_thread_fn)
+            #self.defog_on_pub_thread.start()
+#
+            #self.handle_toggle_defog(SetBoolRequest(False))
 
     def __str__(self):
         """Return string representation."""
         return(self.hostname + ',' + self.username + ',' + self.password +
                        '(' + str(self.width) + 'x' + str(self.height) + ')')
 
-    def peer_subscribe(self, topic_name, topic_publish, peer_publish):
-        '''Lazy-start the image-publisher.'''
-        if self.st is None:
-            self.st = StreamThread(self)
-            self.st.start()
+    
 
     def handle_toggle_ir(self, req):
         """Turn the IR mode on/off (if supported)"""
@@ -262,7 +301,7 @@ class Axis:
             resp.message = f"IR mode is {on_off[req.data]}"
             self.ir_on = req.data
         except Exception as err:
-            rospy.logwarn(f"Failed to set IR mode: {err}")
+            self.get_logger().warn(f"Failed to set IR mode: {err}")
             ok = False
             resp.message = str(err)
 
@@ -302,7 +341,7 @@ class Axis:
             resp.message = f"Wiper is {on_off[self.wiper_on]}"
             self.wiper_on = req.data
         except Exception as err:
-            rospy.logwarn(f"Failed to set wiper mode: {err}")
+            self.get_logger().warn(f"Failed to set wiper mode: {err}")
             resp.success = False
             resp.message = str(err)
         return resp
@@ -344,7 +383,7 @@ class Axis:
             resp.message = f"Defogger is {on_off[self.defog_on]}"
             self.defog_on = req.data
         except Exception as err:
-            rospy.logwarn(f"Failed to set defogger mode: {err}")
+            self.get_logger().warn(f"Failed to set defogger mode: {err}")
             resp.success = False
             resp.message = str(err)
         return resp
@@ -357,47 +396,11 @@ class Axis:
             rate.sleep()
 
 def main():
-    rospy.init_node("axis_driver")
+    rclpy.init(args=sys.argv)
 
-    arg_defaults = {
-        'hostname': '192.168.0.90',       # default IP address
-        'username': 'root',               # default login name
-        'password': '',
-        'width': 640,
-        'height': 480,
-        'fps': 0,                         # frames per second (0 = camera default)
-        'frame_id': 'axis_camera',
-        'camera_info_url': '',
-        'use_encrypted_password' : False,
-        'camera' : 0,
-        'ir': False,
-        'defog': False,
-        'wiper': False }
-    args = updateArgs(arg_defaults)
-    Axis(**args)
-    rospy.spin()
+    axis = Axis()
 
-def updateArgs(arg_defaults):
-    '''Look up parameters starting in the driver's private parameter space, but
-    also searching outer namespaces.  Defining them in a higher namespace allows
-    the axis_ptz.py script to share parameters with the driver.'''
-    args = {}
-    for name, val in arg_defaults.items():
-        full_name = rospy.search_param(name)
-        if full_name is None:
-            args[name] = val
-        else:
-            args[name] = rospy.get_param(full_name, val)
-    # resolve frame_id with tf_prefix (unless already absolute)
-    if args['frame_id'][0] != '/':        # not absolute?
-        tf_prefix = rospy.search_param('tf_prefix')
-        prefix_val = ''
-        if tf_prefix is not None:           # prefix defined?
-            prefix_val = rospy.get_param(tf_prefix)
-            if prefix_val[0] != '/':          # prefix not absolute?
-                prefix_val = '/' + prefix_val
-        args['frame_id'] = prefix_val + '/' + args['frame_id']
-    return(args)
+    rclpy.spin(axis)
 
 if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
-from setuptools import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import find_packages, setup
 
-d = generate_distutils_setup(
-    install_requires=['rospy', 'urllib'],
-    )
+package_name = 'axis_camera'
 
-setup(**d)
+setup(
+    name=package_name,
+    version='0.4.3',
+    packages=['nodes'],
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Jason Kelly',
+    maintainer_email='jasonk@fremont.ai',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    entry_points={
+        'console_scripts': [
+             'axis = nodes.axis:main',
+        ],
+    },
+)
