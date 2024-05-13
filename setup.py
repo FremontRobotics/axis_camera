0200e6abd0232fda809837070a4f338dcc9bce48
Jason Kelly <3310379+JasonLKelly@users.noreply.github.com>
2024-05-13 12:25:14 -0700

    fix the freaking build

--- setup.py
+++ setup.py
@@ -7,26 +7,27 @@ package_name = 'axis_camera'
 setup(
     name=package_name,
     version='0.4.3',
-    packages=['nodes'],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch')),
-    ],
+
+     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Jason Kelly',
-    maintainer_email='jasonk@fremont.ai',
+    maintainer_email='3310379+JasonLKelly@users.noreply.github.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     entry_points={
         'console_scripts': [
-             'axis = nodes.axis:main',
-             'axis_ptz = nodes.axis_ptz:main',
-             'axis_teleop = nodes.teleop:main',
-             'axis_teleop_speed_control = nodes.teleop_speed_control:main',
-             'publish_axis_tf = nodes.publish_axis_tf:main'
+             'axis = axis_camera.axis:main',
+             'axis_ptz = axis_camera.axis_ptz:main',
+             'axis_teleop = axis_camera.teleop:main',
+             'axis_teleop_speed_control = axis_camera.teleop_speed_control:main',
+             'publish_axis_tf = axis_camera.publish_axis_tf:main'
         ],
     },
 )
